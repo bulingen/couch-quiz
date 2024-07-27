604c05b6b1525088ec811aaaf2fa39603b0f08c5
bulingen <adamlecorney@gmail.com>
2024-07-27 21:58:12 +0200

    chore: typescript

---
+++ app.config.ts
@@ -0,0 +1,30 @@
+import "ts-node/register"; // Add this to import TypeScript files
+import { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "couch-quiz",
+  slug: "couch-quiz",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+};
+
+export default config;
